8ffc2153498f0258774ebc6c1b477c06c4280221
rafaeltorres20 <dev.rafael.torres@gmail.com>
2023-10-18 09:47:22 -0300

    feat: adding a compiler

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,5 +1,39 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
 // index.ts
-import html2canvas from "html2canvas";
+var useScreenchot_exports = {};
+__export(useScreenchot_exports, {
+  handlePrintScreen: () => handlePrintScreen
+});
+module.exports = __toCommonJS(useScreenchot_exports);
+var import_html2canvas = __toESM(require("html2canvas"), 1);
 
 // constants.ts
 var CONSTANTS = {
@@ -31,7 +65,7 @@ async function handlePrintScreen(props) {
   let initY = 0;
   let base64PrintScreen = "";
   const root = document.getElementById(props.rootElementID);
-  const rootCanvas = await html2canvas(root, DEFAULTS);
+  const rootCanvas = await (0, import_html2canvas.default)(root, DEFAULTS);
   rootCanvas.style.position = CONSTANTS.position;
   rootCanvas.style.top = CONSTANTS.top;
   rootCanvas.style.left = CONSTANTS.left;
@@ -115,7 +149,8 @@ async function handlePrintScreen(props) {
   });
   return base64PrintScreen;
 }
-export {
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
   handlePrintScreen
-};
-//# sourceMappingURL=index.js.map
+});
+//# sourceMappingURL=index.cjs.map
